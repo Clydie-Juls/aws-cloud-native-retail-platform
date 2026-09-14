4622c7c3091a0db38ecd69d8389ca39c9b388edf
clydie-juls <clyde_marindo@dlsu.edu.ph>
2026-09-14 07:39:07 -0600

    add microservices container definitions

--- locals.tf
+++ locals.tf
@@ -6,4 +6,54 @@ locals {
   azs             = slice(data.aws_availability_zones.available.names, 0, 3)
   public_subnets  = [for k, az in local.azs : cidrsubnet(var.vpc_cidr, var.subnet_newbits, k)]
   private_subnets = [for k, az in local.azs : cidrsubnet(var.vpc_cidr, var.subnet_newbits, k + 10)]
+
+  cart_containers = concat(
+    jsondecode(file("${path.module}/task-definitions/cart-service.json")),
+    jsondecode(file("${path.module}/task-definitions/cart-db.json"))
+  )
+
+  catalog_containers = concat(
+    jsondecode(templatefile(
+      "${path.module}/task-definitions/catalog-service.json.tftpl",
+      {
+        db_password = var.db_password
+      }
+    )),
+    jsondecode(templatefile(
+      "${path.module}/task-definitions/catalog-db.json.tftpl",
+      {
+        db_password = var.db_password
+      }
+    ))
+  )
+
+  checkout_containers = concat(
+    jsondecode(file("${path.module}/task-definitions/checkout-service.json")),
+    jsondecode(file("${path.module}/task-definitions/checkout-db.json"))
+  )
+
+  orders_containers = concat(
+    jsondecode(templatefile(
+      "${path.module}/task-definitions/order-service.json.tftpl",
+      {
+        db_password = var.db_password
+      }
+    )),
+    jsondecode(templatefile(
+      "${path.module}/task-definitions/order-db.json.tftpl",
+      {
+        db_password = var.db_password
+      }
+    )),
+    jsondecode(templatefile(
+      "${path.module}/task-definitions/orders-rabbitmq.json.tftpl",
+      {
+        db_password = var.db_password
+      }
+    ))
+  )
+
+  ui_containers = jsondecode(
+    file("${path.module}/task-definitions/ui-service.json")
+  )
 }
